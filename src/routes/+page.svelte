56fd9464b72d13aae2f81076c693d15cb603380a
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-02-25 19:46:38 +0900

    htmlタグのlang属性をjaに変更し、descriptionを記述し、URLをlocalhost:3000に設定

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  
+  onMount(async () => {
+    const res =  await fetch(`${import.meta.env.VITE_API_URL}/api/user`, {
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      mode: "cors",
+      credentials: "include"
+    });
+    console.log(await res.json());
+  });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
