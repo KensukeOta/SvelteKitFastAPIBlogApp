a64bf3d3220a04069e881b1c656d6e9218f624c9
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-07-26 19:55:45 +0900

    ヘッダーとフッターを実装

---
+++ src/lib/components/organisms/Header/Header.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+</script>
+
+<header class="flex justify-between items-center border-b relative">
+  <div class="leading-9">
+    <button class="px-2 rounded-full hover:bg-gray-200">
+      <i class="bi bi-list"></i>
+    </button>
+    <h1 class="inline-block">
+      <a href="/" class="inline-block">
+        <enhanced:img
+          src="$lib/images/favicon.png"
+          alt="Svelte Logo"
+          class="inline-block h-6 w-6 object-cover"
+        />
+      </a>
+    </h1>
+  </div>
+
+  <button type="button" title="検索" class="absolute w-4 right-0 left-0 mx-auto h-full">
+    <i class="bi bi-search"></i>
+  </button>
+
+  <nav class="leading-9">
+    <a href="/signup" class="inline-block">新規登録</a>
+    <a href="/login" class="inline-block">ログイン</a>
+  </nav>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
   import "../app.css";
+  import "bootstrap-icons/font/bootstrap-icons.css";
+  import Header from "$lib/components/organisms/Header/Header.svelte";
+	import Footer from "$lib/components/organisms/Footer/Footer.svelte";
 </script>
 
-<slot></slot>
+<Header />
+
+<main class="flex-1">
+  <slot></slot>
+</main>
+
+<Footer />
