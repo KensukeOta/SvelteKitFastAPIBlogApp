66446985947d5d8ce1f495d0366ecff645d67f29
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-08-02 12:00:35 +0900

    未ログイン時や、ログインユーザーと記事の著者が違う場合や、記事の著者名とURLの著者名が違う場合に記事更新ページに遷移しようとしたら、トップページにリダイレクトするよう実装 (#7)

--- src/routes/(authenticated)/[name]/posts/[id]/edit/+page.server.ts
+++ src/routes/(authenticated)/[name]/posts/[id]/edit/+page.server.ts
@@ -13,6 +13,7 @@ const schema = z.object({
 
 export const load: PageServerLoad = async ({ fetch, params, parent }) => {
   const { session } = await parent();
+
   let post;
 
   try {
@@ -35,7 +36,7 @@ export const load: PageServerLoad = async ({ fetch, params, parent }) => {
 
   const form = await superValidate(post, zod(schema));
 
-  if (!session) {
+  if (!session || session.user?.id !== post.user.id || post.user.name !== params.name) {
     redirect(303, "/");
   }
 
