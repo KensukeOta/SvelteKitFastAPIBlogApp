6b3429272ef3e83b452e9cf1549fd79d7f084561
Kensuke Ota <40653830+KensukeOta@users.noreply.github.com>
2024-08-05 08:56:56 +0900

    ログイン機能を実装 (#14)

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,49 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Google from "@auth/sveltekit/providers/google";
 import GitHub from "@auth/sveltekit/providers/github";
+import Credentials from "@auth/sveltekit/providers/credentials"
 import { API_URL } from "$env/static/private";
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   providers: [
     Google,
     GitHub,
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        try {
+          let user = null;
+
+          const email = credentials.email;
+          const password = credentials.password;
+
+          const res = await fetch(`${API_URL}/v1/sessions`, {
+            method: "POST",
+            headers: {
+              "Accept": "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password, provider: "credentials" }),
+          })
+          if (!res.ok) {
+            const errors = await res.json();
+            console.log(errors);
+            throw new Error();
+          }
+          user = await res.json()
+          console.log(user)
+          // return user object with the their profile data
+          return user
+        } catch (error) {
+          return null
+        }
+      },
+    }),
   ],
 
   callbacks: {
@@ -33,6 +70,10 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
       return session;
     },
     async signIn({ user, account }) {
+      if (account?.provider === "credentials") {
+        return true;
+      }
+      
       const name = user.name;
       const email = user.email;
       const image = user.image;
--- src/routes/(guest)/login/+page.svelte
+++ src/routes/(guest)/login/+page.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
-  import OAuthMenu from "$lib/components/molecules/OAuthMenu/OAuthMenu.svelte";
+  import { signIn } from "@auth/sveltekit/client";
+  import { superForm } from "sveltekit-superforms";
+	import OAuthMenu from "$lib/components/molecules/OAuthMenu/OAuthMenu.svelte";
 	import DefaultLayout from "$lib/components/templates/DefaultLayout/DefaultLayout.svelte";
+
+	let { data } = $props();
+
+  let error = $state("");
+  
+  const { form, errors, constraints, message, enhance, submitting } = superForm(data.form, {
+    resetForm: false,
+  });
+
+  const handleSubmit = async (e: SubmitEvent) => {
+    e.preventDefault();
+    await signIn("credentials", { email: $form.email, password: $form.password });
+  }
 </script>
 
 <svelte:head>
@@ -8,9 +23,61 @@
 </svelte:head>
 
 <DefaultLayout className="py-4">
-  <div class="h-full flex flex-col items-center justify-center gap-2 text-center">
-    <div class="h-full flex flex-col items-center justify-center gap-2 bg-white w-80 px-8 py-5">
-      <OAuthMenu />
-    </div>
-  </div>
+	<div class="h-full flex flex-col items-center justify-center gap-2 text-center">
+		<div class="h-full flex flex-col items-center justify-center gap-2 bg-white w-80 px-8 py-5">
+      <form method="POST" onsubmit={handleSubmit} class="w-full" use:enhance>
+        <div id="create-error" aria-live="polite" aria-atomic="true">
+          {#if error}
+            <p class="text-red-500">
+              {error}
+            </p>
+          {/if}
+        </div>
+
+        <label for="email" class="block">メールアドレス</label>
+        <input 
+          name="email" 
+          id="email" 
+          type="email"
+          aria-invalid={$errors.email ? 'true' : undefined}
+          bind:value={$form.email}
+          {...$constraints.email}
+          class="border px-4 py-2 w-full" 
+        />
+        <div id="email-error" aria-live="polite" aria-atomic="true">
+          {#if $errors.email}
+            <p class="text-red-500">
+              {$errors.email}
+            </p>
+          {/if}
+        </div>
+
+        <label for="password" class="block">パスワード</label>
+        <input 
+          name="password" 
+          id="password" 
+          type="password"
+          aria-invalid={$errors.password ? 'true' : undefined}
+          bind:value={$form.password}
+          {...$constraints.password}
+          class="border px-4 py-2 w-full" 
+        />
+        <div id="password-error" aria-live="polite" aria-atomic="true">
+          {#if $errors.password}
+            <p class="text-red-500">
+              {$errors.password}
+            </p>
+          {/if}
+        </div>
+
+        <button type="submit" disabled={$submitting} class="w-full p-2 mt-2 bg-black text-white hover:opacity-70">ログイン</button>
+      </form>
+
+      <div>
+        <span class="text-xs">アカウントをお持ちではないですか？</span>
+        <a href="/signup" class="text-xs text-blue-500 hover:opacity-70">新規登録</a>
+      </div>
+			<OAuthMenu />
+		</div>
+	</div>
 </DefaultLayout>
