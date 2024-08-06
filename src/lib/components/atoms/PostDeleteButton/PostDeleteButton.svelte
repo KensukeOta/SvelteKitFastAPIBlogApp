6d5cb5096f5ad03b23fb5eb0dba0e29ebf8da4df
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2024-08-06 13:13:11 +0900

    記事削除処理をクライアントではなくサーバー側で行うよう変更

--- src/lib/components/atoms/PostDeleteButton/PostDeleteButton.svelte
+++ src/lib/components/atoms/PostDeleteButton/PostDeleteButton.svelte
@@ -1,42 +1,18 @@
 <script lang="ts">
   import type { Post } from "$lib/types/Post";
-  import { PUBLIC_API_URL } from "$env/static/public";
-  import { invalidateAll } from "$app/navigation";
-
-  let isSubmitting = $state(false);
+	import type { SubmitFunction } from "@sveltejs/kit";
+	import { enhance } from "$app/forms";
 
   let { post }: { post: Post } = $props();
 
-  const handleSubmit = async (event: SubmitEvent) => {
+  const handleSubmit: SubmitFunction = ({ cancel }) => {
     if (!confirm("この記事を削除しますか？")) {
-      return;
-    }
-    event.preventDefault();
-
-    isSubmitting = true;
-
-    try {
-      const res = await fetch(`${PUBLIC_API_URL}/v1/posts/${post.id}`, {
-        method: "DELETE",
-        headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-      });
-      if (!res.ok) {
-        const errors = await res.json();
-        throw new Error(errors);
-      }
-      await invalidateAll();
-    } catch (error) {
-      console.log(error);
-    } finally {
-      isSubmitting = false;
+      cancel();
     }
   };
 </script>
 
-<form onsubmit={handleSubmit} class="inline-block">
-  <button type="submit" disabled={isSubmitting} class="text-red-500">削除</button>
+<form method="POST" action="/?/delete" class="inline-block" use:enhance={handleSubmit}>
+  <input type="hidden" name="post" value={post.id} />
+  <button type="submit" class="text-red-500">削除</button>
 </form>
--- src/routes/(guest)/+page.server.ts
+++ src/routes/(guest)/+page.server.ts
@@ -1,5 +1,6 @@
-import type { PageServerLoad } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
 import { API_URL } from "$env/static/private";
+import { fail, redirect } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async ({ fetch, parent }) => {
   const { session } = await parent();
@@ -25,3 +26,36 @@ export const load: PageServerLoad = async ({ fetch, parent }) => {
     session,
   }
 };
+
+export const actions = {
+  delete: async ({ fetch, request }) => {
+    const data = await request.formData();
+
+    const postId = data.get("post");
+
+    let errors;
+
+    try {
+      const res = await fetch(`${API_URL}/v1/posts/${postId}`, {
+        method: "DELETE",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        }
+      });
+      if (!res.ok) {
+        errors = await res.json();
+        console.log(errors);
+        throw new Error(errors);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    if (errors) {
+      return fail(400, { postId, errors });
+    }
+
+    redirect(303, "/");
+  },
+} satisfies Actions;
